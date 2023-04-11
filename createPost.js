6695787c01fb6f2a2f340dc57e62644aaef124c1
Miles <mlsmzk02@gmail.com>
2023-04-10 20:49:22 -0400

    Attempt #3 to fix post bug

--- createPost.js
+++ createPost.js
@@ -1,4 +1,4 @@
-async function Post(db, caption, slides, files) {
+async function Post(kdb, caption, slides, files) {
     let db = await Connection.open(mongoUri, kdb);
     // here's the one line that does the query
     let result = await db.collection('posts').insertOne({"id" : postId,
